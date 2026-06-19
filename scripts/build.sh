ec634e62e0d6a1a91707255ad4687c270a651390
Reshma <reshmarethu2504@gmail.com>
2026-06-19 15:38:00 +0530

    enforce immutable image tags, remove mutable stable tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,16 +25,14 @@ echo "Tagging frontend image..."
 docker tag frontend-app:$IMAGE_TAG \
   $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$FRONTEND_REPO:$IMAGE_TAG
 
-docker tag frontend-app:$IMAGE_TAG \
-  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$FRONTEND_REPO:stable
+
 
 echo "Tagging backend image..."
 
 docker tag backend-app:$IMAGE_TAG \
   $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BACKEND_REPO:$IMAGE_TAG
 
-docker tag backend-app:$IMAGE_TAG \
-  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BACKEND_REPO:stable
+
 
 echo "Pushing frontend image..."
 
